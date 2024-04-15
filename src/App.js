0b9c9ee11340322e1a5302246c8f4bb143e856e1
NGUYENDUCTHINH00 <ducthinhnd2004@gmail.com>
2024-04-15 17:43:54 +0700

    services-printerfix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/HomePage.jsx";
 import DetailProduct from "./components/user/DetailProduct/DetailProduct";
+import PrinterRepair from "./components/user/printerRepair/PrinterRepair";
 
 function App() {
   return (
     // <Routes>
     //   <Route path='/' element={<HomePage />}></Route>
     // </Routes>
-    <DetailProduct />
+    // <DetailProduct />
+    <PrinterRepair />
   );
 }
 
